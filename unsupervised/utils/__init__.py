73a07b437945e691ed0f5aec7130cf411ac69d2d
Werner M. Heigl <32463325+wmheigl@users.noreply.github.com>
2020-04-10 10:49:30 -0500

    Add files via upload

---
+++ unsupervised/utils/__init__.py
@@ -0,0 +1,4 @@
+from .plot import *
+
+# exported names at subpackage level
+__all__ = ['plot_data', 'plot_data_hist', 'plot_data_matrix']
---
+++ unsupervised/utils/plot.py
@@ -0,0 +1,118 @@
+'''
+Created on Apr 8, 2020
+
+@author: W. M. Heigl
+'''
+import matplotlib.pyplot as plt
+
+# exported names at module level
+__all__ = ['plot_data', 'plot_data_hist', 'plot_data_matrix']
+
+
+def plot_data(data=None, labels=None, title='Data', show_plot=False):
+    """Plots data.
+    
+    Arguments:
+        data : NumPy ndarray.
+            Each row is a feature vector.
+        labels : NumPy ndarray
+            Feature labels.
+        title : String
+        show_plot : Bool
+            Calls plt.show() if True.
+    """
+    if data is None:
+        print('No data supplied')
+        return
+
+    # need some logic to determine number of rows and columns
+    n_rows = 1
+    n_plots = data.shape[1]
+    fig_size = (2 * 6.4, 4.8)
+    figure, axes = plt.subplots(nrows=n_rows, ncols=n_plots, figsize=fig_size, constrained_layout=True)
+    figure.suptitle(title)
+    for index in range(n_plots):
+        axes[index].plot(data[:, index])
+        if labels is None:
+            axes[index].set_title(index)
+        else:
+            axes[index].set_title(labels[index])          
+    if show_plot is True:
+        plt.show()
+
+
+def plot_data_hist(data=None, bins=50, labels=None, title='Data Histograms', show_plot=False):
+    """Plots data histograms.
+    
+    Arguments:
+        data : NumPy ndarray.
+            Each row is a feature vector.
+        bins : Integer
+            Number of bins in histograms.
+        labels : NumPy ndarray
+            Feature labels.
+        title : String
+        show_plot : Bool
+            Calls plt.show() if True.
+    """
+    if data is None:
+        print('No data supplied')
+        return
+
+    # need some logic to determine number of rows and columns
+    n_rows = 1
+    n_plots = data.shape[1]
+    fig_size = (2 * 6.4, 4.8)
+    figure, axes = plt.subplots(nrows=n_rows, ncols=n_plots, figsize=fig_size, constrained_layout=True)
+    figure.suptitle(title)
+    for index in range(n_plots):
+        axes[index].hist(data[:, index], bins=bins)
+        if labels is None:
+            axes[index].set_title(index)
+        else:
+            axes[index].set_title(labels[index])          
+    if show_plot is True:
+        plt.show()
+
+
+def plot_data_matrix(data=None, labels=None, title='Data Histograms', show_plot=False):
+    """Plots data histograms.
+    
+    Arguments:
+        data : NumPy ndarray.
+            Each row is a feature vector.
+        bins : Integer
+            Number of bins in histograms.
+        labels : NumPy ndarray
+            Feature labels.
+        title : String
+        show_plot : Bool
+            Calls plt.show() if True.
+    """
+    if data is None:
+        print('No data supplied')
+        return
+    
+    # need some logic to determine number of rows and columns
+    n_rows = data.shape[1]
+    n_plots = data.shape[1]
+    fig_size = (2 * 6.4, 2 * 4.8)
+    figure, axes = plt.subplots(nrows=n_rows, ncols=n_plots, figsize=fig_size, constrained_layout=True)
+    figure.suptitle(title)
+    for row in range(n_plots):
+        for col in range(n_plots):
+            axes[row, col].scatter(data[:, row], data[:, col], marker='.', s=2)
+            axes[row, col].set_xlabel('', visible=False)
+            axes[row, col].set_ylabel('', visible=False)
+            axes[row, col].set_yticklabels([])
+            axes[row, col].set_xticklabels([])
+    if labels is None:
+        for i in range(n_plots):
+            axes[0, i].set_title(i)
+            axes[i, 0].set_ylabel(i, visible=True)
+    else:
+        for i in range(n_plots):
+            axes[0, i].set_title(labels[i])
+            axes[i, 0].set_ylabel(labels[i], visible=True)
+    if show_plot is True:
+        plt.show()
