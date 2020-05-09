f36d3bc3a66486658525b5d8552f7892a206f3a4
Werner M. Heigl <werner.heigl@me.com>
2020-05-09 06:32:06 +0200

    Fixes indentation. Minor refactoring.

--- unsupervised/autoencoder/encoder.py
+++ unsupervised/autoencoder/encoder.py
@@ -3,11 +3,11 @@ Created on May 8, 2020
 
 @author: wernerheigl
 '''
-from unsupervised import autoencoder
 
-__all__ = ['Encoder', 'Decoder', 'Autoencoder']
+__all__ = ['Encoder', 'Decoder', 'Autoencoder', 'loss', 'train', 'train_loop']
 __author__ = 'Werner M. Heigl'
 
+import tensorflow as tf
 import tensorflow.keras as k
 
 
@@ -41,6 +41,7 @@ class Decoder(k.layers.Layer):
         activation = self.output_layer(encoded)
         return activation
 
+
 class Autoencoder(k.Model):
     '''
     classdocs
@@ -53,8 +54,31 @@ class Autoencoder(k.Model):
         self.decoder = Decoder(hidden_dim=hidden_dim, original_dim=original_dim)
     
     def call(self, input_features):
-        encoded  = self.encoder(input_features)
+        encoded = self.encoder(input_features)
         reconstructed = self.decoder(encoded)
         return reconstructed
+
     
-        
+def loss(preds, real):
+    return tf.reduce_mean(tf.square(tf.subtract(preds, real)))
+
+
+def train(loss, model, opt, original):
+    with tf.GradientTape() as tape:
+        preds = model(original)
+        reconstruction_error = loss(preds, original)
+        gradients = tape.gradient(reconstruction_error, model.trainable_variables)
+        gradient_variables = zip(gradients, model.trainable_variables)
+    opt.apply_gradients(gradient_variables)
+    return reconstruction_error
+
+
+def train_loop(model, opt, loss, dataset, epochs=20):
+    for epoch in range(epochs):
+        epoch_loss = 0
+        for step, batch_features in enumerate(dataset):
+            loss_values = train(loss, model, opt, batch_features)
+            epoch_loss += loss_values
+        model.loss.append(epoch_loss)
+        print(f'Epoch {epoch + 1}/{epochs}. Loss: {epoch_loss.numpy()}')
+
