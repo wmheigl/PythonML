d2c6fc47e88dd0d1bf66aa6b13535a057aaaab59
Werner M. Heigl <werner.heigl@me.com>
2020-05-09 14:52:44 +0200

    Adds RestrictedBoltzmannMachine.

--- unsupervised/__init__.py
+++ unsupervised/__init__.py
@@ -1,9 +1,11 @@
 from .autoencoder import *
+from .restricted_boltzmann_machine import *
 from .self_organizing_map import *
 from .utils import *
 
 # exported names at package level
 __all__ = ['SelfOrganizingMap', 'WTU',
+           'RestrictedBoltzmannMachine',
            'Encoder', 'Decoder', 'AutoEncoder',
            'SparseEncoder', 'SparseDecoder', 'SparseAutoEncoder',
            'loss', 'train', 'train_loop',
